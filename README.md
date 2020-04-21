bf82430dc7266a593edc913dd5fa357f1d7bfb83
Daniel Ramos <daniel.framos@cassi.com.br>
2020-04-21 17:59:32 -0300

    Melhorado readme

--- README.md
+++ README.md
@@ -6,7 +6,13 @@ Mostro como conectar, além de como trazer dados do usuário logado
 Aqui uso o keycloak-js para conectar no Rhsoo. Estou configurando um exemplo com adapter OIDC;
 Vou criar um branch quando terminar o exemplo com adapter OIDC e coloco aqui
 
+Usamos 2x fluxos:
+
+- Usuário de servico (exemplo-iron). Adapters js terceiro não dao suporte ao fluxo client_credentials. Este fluxo foi criado para cenários de aplicações legadas, ou integração sistemica. Logo, fazemos um fetch para gerar o token
+- Usuário normal (osvaldo.melo). Aqui, usamos o adapter js para gerar token, bem como pegar informações extras do usuário
+  
 Qualquer duvida, entrar em contato comigo ou Osvaldo
+
 
 ## Install
 ```
--- src/main.js
+++ src/main.js
@@ -18,6 +18,28 @@ Vue.use(VueLogger, options);
 
 //keycloak init options
 
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+var urlencoded = new URLSearchParams();
+urlencoded.append("grant_type", "client_credentials");
+urlencoded.append("client_id", "exemplo-iron");
+urlencoded.append("client_secret", "7878175c-b702-4ab6-98f3-ab9a3a9e2027");
+
+var requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: urlencoded,
+  redirect: "follow",
+};
+
+fetch(
+  "https://rhsso-np.paas.cassi.com.br/auth/realms/Colaborador/protocol/openid-connect/token",
+  requestOptions
+)
+  .then((response) => response.json())
+  .then((result) => Vue.$log.info("Token de usuário de servico", result));
+
 let keycloak = Keycloak("/keycloak.json");
 
 keycloak
@@ -29,22 +51,22 @@ keycloak
     if (!auth) {
       alert("Autenticação fallhou. Verifique o json e o flow do oauth");
     } else {
-      Vue.$log.info("Authenticated");
-      Vue.$log.info("Token: " + keycloak.token);
-
-      alert("Seu token : " + keycloak.token);
+      Vue.$log.info(
+        "Token de usuário autenticado autenticado: ",
+        keycloak.token
+      );
     }
-
-    // TODO: Maybe dont store the token in the localstore, rather use it direct from the keycloak.token object
-    localStorage.setItem("vue-token", keycloak.token);
-    localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
 
     return keycloak.loadUserInfo();
   })
   .then((userInfo) => {
     // Retorna os dados do usuário
+    // Já que autenticamos com usuário com senha, vamos pegar dados dele
 
-    Vue.$log.info(userInfo);
+    Vue.$log.info(
+      "Informações de usuário autenticado de usuário autenticado:",
+        userInfo
+    );
     new Vue({
       render: (h) => h(App),
     }).$mount("#app");
